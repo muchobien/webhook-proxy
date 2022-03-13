8bc6fdfd97bf25b0c960f3e67eddd6fc1f96f09e
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2022-03-13 11:45:04 +0100

    fix: get service as query param

--- main.go
+++ main.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/muchobien/webhook-proxy/proxy"
 )
 
-const (
-	// ServicesURI is the URI for the services
-	ServicesURI string = "https://gist.githubusercontent.com/KROSF/22dc413f61212be0677efbf04f3a1c66/raw/services.json"
-)
-
 func ProxyWebHooks(c *fiber.Ctx) error {
-	agent := fiber.AcquireClient()
+	service := c.Query("service")
 
-	services := make(map[string]string)
-	agent.Get(ServicesURI).Struct(&services)
-	service, ok := services[c.Params("service")]
-
-	if !ok {
-		return c.SendStatus(404)
+	_, err := url.ParseRequestURI(service)
+	if err != nil {
+		return c.SendStatus(400)
 	}
 
 	if err := proxy.Do(c, service); err != nil {
@@ -40,7 +33,7 @@ func main() {
 		return c.SendString("Welcome to the Webhook Proxy!")
 	})
 
-	app.Post("/wh/:service", ProxyWebHooks)
+	app.Post("/wh", ProxyWebHooks)
 
 	app.Listen(fmt.Sprintf(":%s", port))
 }
